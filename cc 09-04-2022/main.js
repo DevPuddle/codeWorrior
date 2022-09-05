36eb5bc40317cede5c77e1f9b8dda807d28562bd
Isaiah Henson <Devpuddles@gmail.com>
2022-09-04 20:31:57 -0400

    coding challenge for 09-05-2022

---
+++ cc 09-04-2022/main.js
@@ -0,0 +1,15 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+//PREP
+//P: Input should be a template literal
+//R: the function should return a value
+//E:  function greet (name) {
+// return `Hello ${name} how are you doing today?`
+//}
+//P: call thing the function name(Isaiah)
+
+function greet(name){
+    return `Hello ${name} how are you doing today?`
+  }
---
+++ cc 09-05-2022/main.js
@@ -0,0 +1,24 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//P: input will be a true or false conditional
+//R: This function will return a string value
+//E: function trueorfalse(x){
+//   
+// P: 
+
+//Over thinking below this is the correct answer.
+function booleanToString(b) {
+    if (b) {
+        return b.toString();
+    } else {
+        return b.toString();
+    }
+}
+
+// Correct Answer
+function booleanToString(b) {
+    return b.toString()
+}
+
