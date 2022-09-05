4fb938d147ff5e4ecf249766de3a0ccf444721cf
Isaiah Henson <Devpuddles@gmail.com>
2022-09-04 20:59:04 -0400

    coding challenge for 09-05-2022

--- cc 09-05-2022/main.js
+++ cc 09-05-2022/main.js
@@ -22,3 +22,35 @@ function booleanToString(b) {
     return b.toString()
 }
 
+//New coding challenge
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+//P: A string will be passed into the function
+//R: This function will return a shortend string
+//E: function abname(n) name need to be put into an array, return the first initials capitalized sep by period
+//P: return name.split(' ')
+
+function abbrevName(name){
+    let arr = name.split(' ')
+    arr.forEach(element => {
+        return element.charAt(0).toUpperCase() + '.'
+    });
+}  
+console.log(abbrevName('Isaiah Henson'))
+console.log(arr)
+
+//Correct answer is: 
+//
+function abbrevName(name){
+    return name[0].toUpperCase() + "." + name[name.indexOf(' ')+1].toUpperCase();
+
+}  
