76c348c0ec54e8db4571cb15c94604d8295228ef
Isaiah Henson <Devpuddles@gmail.com>
2022-10-22 08:08:36 -0400

    coding challenge for 10-21-2022

---
+++ cc 10-21-2022/main.js
@@ -0,0 +1,11 @@
+// Write a function which converts the input string to uppercase.
+
+// function will take a string
+// function will ruturn the string uppercase
+// "Isaiah Henson" --> 'ISAIAH HENSON'
+// Take the string apply toUppercase() to the string
+
+//Be sure to camal case the methoed toUpperCase
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
