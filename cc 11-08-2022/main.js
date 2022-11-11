0d6c9399222232a8ee67fc0cb202145a2c880b4e
Isaiah Henson <Devpuddles@gmail.com>
2022-11-10 21:48:30 -0500

    november tenth  commit

--- cc 11-08-2022/main.js
+++ cc 11-08-2022/main.js
@@ -15,6 +15,53 @@
 // 1705 --> 18
 // if lst number is one or high round up else if 00 less
 
-function century(year) {
-    return Math.ceil(year/100)
+//nswer to november th chllenge 
+
+// function century(year) {
+//     return Math.ceil(year/100)
+//   }
+
+//   Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+//   Division by zero should return NaN.
+  
+//   Examples:
+//   n = 17
+//   m = 5
+//   result = 2 (remainder of `17 / 5`)
+  
+//   n = 13
+//   m = 72
+//   result = 7 (remainder of `72 / 13`)
+  
+//   n = 0
+//   m = -1
+//   result = 0 (remainder of `0 / -1`)
+  
+//   n = 0
+//   m = 1
+//   result - division by zero (refer to the specifications on how to handle this in your language)
+
+// two integers 
+// return the vlue of the big num being devided
+//if num is 20 second num 2 devide the lrger byt the other
+// if num to is greter devide it by other vice vera
+
+
+function remainder(n, m){
+    if (n > m ){
+        return n/m
+    } else {
+        return m/n
+    }
   }
+
+  function remainder(a, b){
+
+    if(b>a)
+      return b%a;
+    else
+      return a%b;
+  }
+
+  remainder(900,1200)
