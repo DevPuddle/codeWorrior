bb00654cdffc9ec8a210c5d4a77f2c2a9f1556fd
Isaiah Henson <Devpuddles@gmail.com>
2022-11-13 12:43:29 -0500

    my attempt at todays challenge

--- cc 11-13-2022/main.js
+++ cc 11-13-2022/main.js
@@ -8,6 +8,21 @@
 
 // if bmi > 30 return "Obese"
 
+//this function will receive number inputs
+//this functin will return a string value
+// if number is above 200 --> return sting "overweight"
+// function overweight(num, height){ if num divited by height above 200 return "overweight" }
+
 function bmi(weight, height) {
-    return "";
+    let bmi = weight / height;
+    if (bmi <= 18.5) {
+        return 'Underweight'
+    } else if (bmi <= 25.0) {
+        return "Normal"
+    } else if (bmi <= 30.0) {
+        return "Overweight"
+    } else if (bmi > 30) {
+        return "Obese"
+    }
   }
+
